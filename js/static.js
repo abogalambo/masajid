c6c563e556158cc46f25ae56c0389038b66e4013
thedecimal <yaser@rubikal.com>
2015-01-02 20:51:19 +0200

    fix bug with ltr navigation

--- js/static.js
+++ js/static.js
@@ -1,5 +1,6 @@
 var currentDir = "rtl"
 var currentPage = 'home'
+var pages = ["home","add","about", "thanks"];
 function switchDir(dir){
 	if(dir != currentDir){
 		if(dir == "rtl"){
@@ -7,10 +8,12 @@ function switchDir(dir){
 			$('head').append(link)
 			$('.text-left').removeClass('text-left').addClass('text-right')
 			currentDir = dir
+			initPages();
 		}else if(dir == "ltr"){
 			$("#rtl-style").remove()
 			$('.text-right').removeClass('text-right').addClass('text-left')
 			currentDir = dir
+			initPages();
 		}
 	}
 }
@@ -24,9 +27,30 @@ $("#main-map").click(function(){
 	toggleMosque();
 });
 
-function showPage(pageID){
-  var pID = pageID || window.location.hash;
-  $('#' + currentPage).animate({left: '-100%'}, 600);
-  $('#' + pID).stop().animate({left:0}, 600);
-  currentPage = pID
+function initPages(){
+	var multiplier = 1;
+	if(currentDir == 'ltr') multiplier = -1;
+	for(var i=0; i< pages.length; i++){
+		$("#" + pages[i]).css('left', (i* -100 * multiplier) + "%");
+	}
+}
+initPages();
+
+$(window).on('hashchange',function(e){ 
+    // $('h1').text(location.hash.slice(1));
+    showPage(location.hash.slice(5));
+    return false;
+});
+
+function showPage(pID){
+	var index = pages.indexOf(pID);
+	if(index == -1) return;
+	var multiplier = 1;
+	if(currentDir == 'ltr') multiplier = -1;
+	for(var i=0; i< pages.length; i++){
+		$("#" + pages[i]).animate({left: 100 * multiplier * (index - i) + "%"});
+	}
+	// $('#' + currentPage).animate({left: '-100%'}, 600);
+	// $('#' + pID).stop().animate({left:0}, 600);
+	currentPage = pID
 }
